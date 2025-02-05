3044430c6015068696ee611345bdbae6b50fcdc7
baej-ultra <pigmejov@gmail.com>
2025-02-05 19:13:29 +0100

    Initial commit

---
+++ src/test/java/org/bromanowski/classbooking/ClassBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bromanowski.classbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
