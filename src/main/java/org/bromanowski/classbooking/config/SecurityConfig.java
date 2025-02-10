33247aa6f80f115161a9ef367727e711b4a9dbfa
baej-ultra <pigmejov@gmail.com>
2025-02-10 16:12:28 +0100

    base for jwt

--- src/main/java/org/bromanowski/classbooking/config/SecurityConfig.java
+++ src/main/java/org/bromanowski/classbooking/config/SecurityConfig.java
@@ -1,33 +1,84 @@
 package org.bromanowski.classbooking.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    private final RsaKeyProperties rsaKeys;
+
+    public SecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("gymtest")
+                        .password("{noop}gymtest")
+                        .authorities("ADMIN")
+                        .build());
+    }
 
-//        http.authorizeHttpRequests(configurer ->
-//                configurer
-//                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-//                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-//                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-//                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-//                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-//        );
+    @Bean
+    @Order(1)
+    SecurityFilterChain basicSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        // use HTTP Basic authentication
-        http.httpBasic(Customizer.withDefaults());
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/token").authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .securityMatcher("/token")
+                .build();
+    }
 
-        // disable CSRF
-        http.csrf(AbstractHttpConfigurer::disable);
+    @Bean
+    @Order(2)
+    SecurityFilterChain tokenSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
